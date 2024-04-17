14d1fb9467ac4d0a6613880f9739834d860df672
Suyog Shrestha <suyogstha317@gmail.com>
2024-04-17 23:26:10 +0545

    api integration

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,6 +6,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // place order
 const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://localhost:5174"
+
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -38,8 +41,18 @@ const placeOrder = async (req, res) => {
             quantity: 1
         })
 
-    } catch (error) {
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items,
+            mode: "payment",
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+        })
 
+        res.json({ success: true, session_url: session.url })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
     }
 }
 
